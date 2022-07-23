6c5100b613c4aba7e93497669db21ca02846b037
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-07-23 12:44:08 +0600

    reorganized

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import Card from "./Card";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -29,7 +29,7 @@ function Expenses(props) {
         date={props.items[3].date}
       />
     </Card>
-  )
-}
+  );
+};
 
 export default Expenses;
