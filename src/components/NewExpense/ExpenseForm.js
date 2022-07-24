6acb4a522487eb916d9a0bc46598c862a58610fe
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-07-25 01:32:18 +0600

    fixed number conversion

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
         event.preventDefault();
         const expenseDate = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
 
