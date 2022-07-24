25aa69a2a5fb4cf3778cc81be58fac913be16bfc
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-07-24 20:53:10 +0600

    create ExpensesList component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,29 @@
-import { useState } from "react";
+import {useState} from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title);
+    const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () =>{
-    setTitle('Update!');
-    console.log('Clicked!!!!');
-  }
+    const clickHandler = () => {
+        setTitle('Update!');
+        console.log('Clicked!!!!');
+    }
 
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-      </div>
-      <div className="expense-item__price">{props.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
-  );
+    return (
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{title}</h2>
+                </div>
+                <div className="expense-item__price">{props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
+            </Card>
+        </li>
+    );
 };
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
@@ -16,24 +16,12 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if(filteredExpenses.length>0){
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-                key={expense.id}
-            />
-        ));
-    }
-
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {expensesContent}
+                <ExpensesList items = {filteredExpenses}/>
+
 
 
                 {/*{filteredExpenses.length === 0 && <p>No expenses found.</p>}*/}
