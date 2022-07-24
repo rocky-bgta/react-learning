b85f00745976333f41bbc63d4dbab0205a807563
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-07-24 20:19:17 +0600

    multiple way of condtion to render jsx code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,18 +12,49 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if(filteredExpenses.length>0){
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                key={expense.id}
+            />
+        ));
+    }
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {props.items.map((expense) => (
-                    <ExpenseItem
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                        key={expense.id}
-                    />
-                ))}
+                {expensesContent}
+
+
+                {/*{filteredExpenses.length === 0 && <p>No expenses found.</p>}*/}
+
+                {/*{filteredExpenses.length > 0 && filteredExpenses.map((expense) => (*/}
+                {/*    <ExpenseItem*/}
+                {/*        title={expense.title}*/}
+                {/*        amount={expense.amount}*/}
+                {/*        date={expense.date}*/}
+                {/*        key={expense.id}*/}
+                {/*    />*/}
+                {/*))}*/}
+
+                {/*{filteredExpenses.length == 0 ? (<p>No expense found.</p>) : filteredExpenses.map((expense) => (*/}
+                {/*    <ExpenseItem*/}
+                {/*        title={expense.title}*/}
+                {/*        amount={expense.amount}*/}
+                {/*        date={expense.date}*/}
+                {/*        key={expense.id}*/}
+                {/*    />*/}
+                {/*))}*/}
             </Card>
         </div>
     );
