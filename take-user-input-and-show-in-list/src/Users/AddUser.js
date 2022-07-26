0e9732090954f4024943dfdd99b05e71ca2c1546
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-07-26 13:32:13 +0600

    use Wrapper instead of div

--- take-user-input-and-show-in-list/src/Users/AddUser.js
+++ take-user-input-and-show-in-list/src/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from '../Helpers/Wrapper'
 
 const AddUser = (props) => {
 
@@ -44,23 +45,25 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
-            {error &&
-                <ErrorModal
-                    title = {error.title}
-                    message={error.message}
-                    onConfirm = {errorHandler}
-                /> }
-            <Card className={classes.input}>
-                <form onSubmit={addUserHandler}>
-                    <label htmlFor="username">Username</label>
-                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
-                    <label htmlFor="age">Age (Years)</label>
-                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
-                    <Button type="submit">Add User</Button>
-                </form>
-            </Card>
-        </div>
+        <Wrapper>
+            <div>
+                {error &&
+                    <ErrorModal
+                        title={error.title}
+                        message={error.message}
+                        onConfirm={errorHandler}
+                    />}
+                <Card className={classes.input}>
+                    <form onSubmit={addUserHandler}>
+                        <label htmlFor="username">Username</label>
+                        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
+                        <label htmlFor="age">Age (Years)</label>
+                        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+                        <Button type="submit">Add User</Button>
+                    </form>
+                </Card>
+            </div>
+        </Wrapper>
     );
 };
 
