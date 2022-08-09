dcca9799974d886ad57552afcac6e8941eab5323
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-08-09 19:39:09 +0600

    validate input with forward Ref

--- react-use-effect/src/components/Login/Login.js
+++ react-use-effect/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useReducer, useContext} from 'react';
+import React, {useState, useEffect, useReducer, useContext, useRef} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -39,6 +39,9 @@ const Login = (props) => {
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, {value: '', isValid: null});
 
     const authCtx = useContext(AuthContext);
+
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
 
     // object destructuring and set alise
     const {isValid: emailIsValid} = emailState;
@@ -96,13 +99,20 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        authCtx.onLogin(emailState.value, passwordState.value);
+        if (formIsValid) {
+            authCtx.onLogin(emailState.value, passwordState.value);
+        } else if (!emailIsValid) {
+            emailInputRef.current.focus();
+        }else{
+            passwordInputRef.current.focus();
+        }
     };
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
                 <Input id="email"
+                       ref={emailInputRef}
                        label="E-Mail"
                        type="email"
                        isValid={emailIsValid}
@@ -111,6 +121,7 @@ const Login = (props) => {
                        onBlur={validateEmailHandler}
                 />
                 <Input id="password"
+                       ref={passwordInputRef}
                        label="Password"
                        type="password"
                        isValid={passwordIsValid}
@@ -119,7 +130,7 @@ const Login = (props) => {
                        onBlur={validatePasswordHandler}
                 />
                 <div className={classes.actions}>
-                    <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+                    <Button type="submit" className={classes.btn}>
                         Login
                     </Button>
                 </div>
